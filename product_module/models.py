a0b34ab2bad10d70ac672cb172671174d22e2c68
Urarichu <aayushh17@gmail.com>
2022-06-06 08:37:36 +0545

    Lab commit

--- product_module/models.py
+++ product_module/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.html import mark_safe
+
 class Brand(models.Model):
     name = models.CharField(max_length=200)
     is_active = models.BooleanField()
@@ -17,5 +19,10 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     registered_on = models.DateTimeField()
     is_active = models.BooleanField()
+    def image_tag(self):
+        return mark_safe(f'<img src="{self.image_url}" width="50"height="50" />')
+    image_tag.short_description = "Product"
+    def __str__(self):
+        return self.name
     
 # Create your models here.
